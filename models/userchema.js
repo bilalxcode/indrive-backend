0ded4fcdef8fbbf050cc66d5b5ac390466972918
Bilal Mumtaz <bilalmumtaz868@gmail.com>
2024-07-05 00:09:13 +0500

    admin complete

--- models/userchema.js
+++ models/userchema.js
@@ -42,7 +42,10 @@ const userSchema = new Schema({
   requestedBy: {
     type: [requestDetailsSchema],
     default: []
-  }
+  },
+  isVerified: { type: Boolean, default: false },
+
+
 });
 
 const User = mongoose.model('User', userSchema);
